beb063fb1e73d0c1bfc74e56f1526a8181fb7412
Bryan Nelson <1914742+bryanjnelson@users.noreply.github.com>
2026-04-08 03:23:36 +0000

    WIP - arrays

---
+++ lessons/arrays/arrays.lesson.js
@@ -0,0 +1,15 @@
+/* creating arrays */
+
+// using the "new" and "Array" keywords
+let minifigureName = new Array('L', 'l', 'o', 'y', 'd', ' ', 'G', 'a', 'r', 'm', 'a', 'd', 'o', 'n');
+
+// using bracket syntax, also called an "array initializer" (more common)
+let minifigureNameTwo = ['Z', 'a', 'n', 'e', ' ', 'J', 'u', 'l', 'i', 'e', 'n'];
+
+// initializing an empty array
+let minifigureNameThree = [];
+
+console.log(minifigureName);
+
+/* accessing arrays */
+let ninjas = ['Lloyd', 'Nya', 'Zane', 'Kai'];
