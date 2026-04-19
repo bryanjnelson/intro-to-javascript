327cdb35d0ed8ade21f115494cc91354c83917f4
Bryan Nelson <1914742+bryanjnelson@users.noreply.github.com>
2026-04-19 03:04:18 +0000

    Updates homework and challenge on conditionals

---
+++ challenges/control-structures-conditionals/control-structures-conditionals.challenge.js
@@ -0,0 +1,8 @@
+/**
+* Using a common theme (pick whatever you like) create the following:
+*
+* 1. An if/else statement that evaluates to true using a boolean AND
+*    comparison operator and logs a message to the console.
+* 2. A switch statement with at least 3 cases and a default case that
+*    logs a message to the console for each case.
+*/
---
+++ homework/control-structures-conditionals/control-structures-conditionals.homework.js
@@ -0,0 +1,8 @@
+/**
+* Using a common theme (pick whatever you like) create the following:
+* 
+* 1. An if statement that evaluates to true and logs a message to the console.
+* 2. An if/else statement that evaluates to false and logs a message to the console.
+*/
+
+// Begin writing your code below
