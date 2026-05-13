62a0625040b713fecea63bfa9b23a5a507839e9c
Bryan Nelson <1914742+bryanjnelson@users.noreply.github.com>
2026-05-13 03:34:56 +0000

    Updates homework on objects

---
+++ homework/objects/objects.homework.js
@@ -0,0 +1,19 @@
+/*
+ * Using the code already written below, complete the following:
+ * 
+ * 1. Create an instance of the HoneyBadger class and assign it to a variable.
+ * 2. Log the name and weight of your HoneyBadger instance to the console.
+ * 3. Create a new property called 'isFierce' in the HoneyBadger class and set it to true for your instance.
+ * 4. Log the 'isFierce' property of your HoneyBadger instance to the console.
+ * 5. Create a method called 'bark' in the HoneyBadger class that logs a message to the console when called.
+ * 6. Call the 'bark' method on your HoneyBadger instance.
+ */
+
+class HoneyBadger {
+    constructor(name, weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+}
+
+// Begin writing your code below
